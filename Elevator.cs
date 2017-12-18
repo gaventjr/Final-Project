c718fd3326354bfd04e602b388b967ef381f30ac
gaventjr <gaventjr@gmail.com>
2017-12-17 20:19:07 -0500

    Initial commit

---
+++ Elevator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            MaxWeight = maxWeight;
+        }
+
+        public void AddOccupants(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            return MaxWeight++;
+        }
+
+        public double IsOverMaxCapacity()
+        {
+            return (GetCurrentWeight);
+        }
+    }
+}
---
+++ Passenger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+
+{
+    class Passenger
+    {
+        private double Weight;
+        private string Name;
+
+        public Passenger(string Name, double Weight)
+        {
+            this.Name = Name;
+            this.Weight = Weight;
+
+        }
+
+        public string GetName()
+
+        {
+            return Name;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+
+    }
+}
